23e97ce34db9ef80821b8d1e8771593c634de271
thangbui-finx <thang.bui@galaxyfinx.com>
2026-02-06 16:51:02 +0700

    wip: fix logo Vikki and get banner from Strapi

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import getBanners from "./src/apis/banners/getBanners";
 import Banner from "./src/components/layout/Banner";
 import Navbar from "./src/components/layout/Navbar";
 import { fetchAPI } from "./src/libs/api";
 
 export default async function Home() {
   const restaurantsData = await fetchAPI("/api/restaurants?populate=thumbnail");
+  const banners = await getBanners();
   return (
     <div>
       <Navbar />
-      <Banner />
+      <Banner banners={banners} />
     </div>
   );
 }
--- app/src/components/layout/Banner.tsx
+++ app/src/components/layout/Banner.tsx
@@ -1,21 +1,26 @@
-export default function Banner() {
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+export default function Banner({ banners }: any) {
   return (
-    <div className="relative w-full">
-      <img
-        src="/banner.jpg"
-        alt="banner"
-        className="w-full h-100 object-cover"
-      />
-      {/* <div className="absolute inset-0 bg-black/20" /> */}
-      <div className="absolute inset-0 flex justify-center flex-col gap-6 p-16">
-        <h1 className="text-3xl">Ưu đãi dành cho khách hàng cá nhân</h1>
-        <div className="w-[65%] text-gray-500">
-          <p className="text-2xl">
-            Khám phá ưu đãi hấp dẫn chỉ dành cho bạn, tận hưởng nhiều giá trị
-            hơn mỗi ngày.
-          </p>
-        </div>
-      </div>
-    </div>
+    <Swiper
+      modules={[Pagination, Autoplay]}
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 5000 }}
+      loop
+    >
+      {banners.map((item: any) => {
+        const imageUrl = process.env.NEXT_PUBLIC_API_URL + item.image.url;
+        return (
+          <SwiperSlide key={item.id}>
+            <div className="relative">
+              <img src={imageUrl} alt="" />
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 }
