85d22166fad71f72a3341ae9e247e6c7d2e1b3a4
thangbui-finx <thang.bui@galaxyfinx.com>
2026-02-09 11:55:29 +0700

    fix: list banners

--- app/src/components/layout/Banner.tsx
+++ app/src/components/layout/Banner.tsx
@@ -3,9 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-export default function Banner({ banners = [] }: any) {
-  console.log("Data banner", banners);
-
+export default function Banner({ banners }: any) {
+  const list = banners ?? [];
   return (
     <Swiper
       modules={[Pagination, Autoplay]}
